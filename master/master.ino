ecc27ffa2de0648faf7d30f5115e41cb8167f89e
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-03 15:58:19 -0300

    parando por aqui

--- master/master.ino
+++ master/master.ino
@@ -1,14 +1,21 @@
 #include <SPI.h> //INCLUSÃO DE BIBLIOTECA
 #include <nRF24L01.h> //INCLUSÃO DE BIBLIOTECA
 #include <RF24.h> //INCLUSÃO DE BIBLIOTECA
+#include <ResponsiveAnalogRead.h>
+
 int Array[4];
+
 RF24 radio(9, 10); //CRIA UMA INSTÂNCIA UTILIZANDO OS PINOS (CE, CSN)     
 const byte endereco[6] = "00001"; //CRIA UM ENDEREÇO PARA ENVIO DOS
 //DADOS (O TRANSMISSOR E O RECEPTOR DEVEM SER CONFIGURADOS COM O MESMO ENDEREÇO)
 
 int pinoBotao = 2; //PINO DIGITAL UTILIZADO PELO BOTÃO
 int estadoBotao = 0; //VARIÁVEL PARA ARMAZENAR O ESTADO DO BOTÃO
- 
+
+const int ANALOG_PIN = A0;
+
+ResponsiveAnalogRead analog(ANALOG_PIN, true);
+
 void setup() {
   pinMode(pinoBotao, INPUT_PULLUP); //DEFINE O PINO COMO ENTRADA / "_PULLUP" É PARA ATIVAR O RESISTOR INTERNO
     //DO ARDUINO PARA GARANTIR QUE NÃO EXISTA FLUTUAÇÃO ENTRE 0 (LOW) E 1 (HIGH)
@@ -19,6 +26,9 @@ void setup() {
 }
  
 void loop(){
+  analog.update();
+  int anguloFiltrado = analog.getValue();
+  int angleFiltradoConvertido=map(anguloFiltradoBase, 0, 1023, 0, 180);
   if(digitalRead(pinoBotao) == HIGH){ //SE A LEITURA DO PINO FOR IGUAL A HIGH, FAZ
     estadoBotao = 1; //VARIÁVEL RECEBE VALOR 1 
     Array[0] = estadoBotao;
