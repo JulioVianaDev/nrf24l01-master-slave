a428f7339a19ca7333a511f877b6e5b6f0b4c5e2
juliovianadev <juliovianadev@mail.com>
2023-05-10 14:06:56 -0300

    teste

--- slave/slave.ino
+++ slave/slave.ino
@@ -1,41 +1,31 @@
 #include <SPI.h> //INCLUSÃO DE BIBLIOTECA
 #include <nRF24L01.h> //INCLUSÃO DE BIBLIOTECA
 #include <RF24.h> //INCLUSÃO DE BIBLIOTECA
-#include <Servo.h>
-
-Servo servo; 
-
-RF24 radio(9, 10); //CRIA UMA INSTÂNCIA UTILIZANDO OS PINOS (CE, CSN)
+ 
+RF24 radio(9, 10); //CRIA UMA INSTÂNCIA UTILIZANDO OS PINOS (CE, CSN)     
 const byte endereco[6] = "00001"; //CRIA UM ENDEREÇO PARA ENVIO DOS
 //DADOS (O TRANSMISSOR E O RECEPTOR DEVEM SER CONFIGURADOS COM O MESMO ENDEREÇO)
-int gotArray[4];
-int pinoLed = 3; //PINO DIGITAL UTILIZADO PELO LED
-int estadoBotao = 1; //VARIÁVEL PARA ARMAZENAR O ESTADO DO BOTÃO
-
+ 
+int pinoBotao = 2; //PINO DIGITAL UTILIZADO PELO BOTÃO
+int estadoBotao = 0; //VARIÁVEL PARA ARMAZENAR O ESTADO DO BOTÃO
+ 
 void setup() {
-  Serial.begin(9600);
-  pinMode(pinoLed, OUTPUT); //DEFINE O PINO COMO SAÍDA
-  digitalWrite(pinoLed, LOW); //LED INICIA DESLIGADO
+  pinMode(pinoBotao, INPUT_PULLUP); //DEFINE O PINO COMO ENTRADA / "_PULLUP" É PARA ATIVAR O RESISTOR INTERNO
+    //DO ARDUINO PARA GARANTIR QUE NÃO EXISTA FLUTUAÇÃO ENTRE 0 (LOW) E 1 (HIGH)
   radio.begin(); //INICIALIZA A COMUNICAÇÃO SEM FIO
-  radio.openReadingPipe(0, endereco);//DEFINE O ENDEREÇO PARA RECEBIMENTO DE DADOS VINDOS DO TRANSMISSOR
-  radio.setPALevel(RF24_PA_HIGH); //DEFINE O NÍVEL DO AMPLIFICADOR DE POTÊNCIA
-  radio.startListening(); //DEFINE O MÓDULO COMO RECEPTOR (NÃO ENVIA DADOS)
-
-  //servo conexão
-  servo.attach(5); 
+  radio.openWritingPipe(endereco); //DEFINE O ENDEREÇO PARA ENVIO DE DADOS AO RECEPTOR
+  radio.setPALevel(RF24_PA_HIGH);  //DEFINE O NÍVEL DO AMPLIFICADOR DE POTÊNCIA
+  radio.stopListening(); //DEFINE O MÓDULO COMO TRANSMISSOR (NÃO RECEBE DADOS)
 }
-
+ 
 void loop(){
-  if (radio.available()){ //SE A COMUNICAÇÃO ESTIVER HABILITADA, FAZ
-    radio.read(&gotArray, sizeof(gotArray)); // LÊ OS DADOS RECEBIDOS
-  if(gotArray[0] == 0){ //SE O PARÂMETRO RECEBIDO (ESTADO ATUAL DO BOTÃO) FOR IGUAL A 0 (PRESSIONADO), FAZ
-    digitalWrite(pinoLed, HIGH); //LIGA O LED
-  }else{
-          if(gotArray[0] == 1){ //SE O PARÂMETRO RECEBIDO (ESTADO ATUAL DO BOTÃO) FOR IGUAL A 1 (NÃO PRESSIONADO), FAZ
-            digitalWrite(pinoLed, LOW); //DESLIGA O LED
+  if(digitalRead(pinoBotao) == HIGH){ //SE A LEITURA DO PINO FOR IGUAL A HIGH, FAZ
+    estadoBotao = 1; //VARIÁVEL RECEBE VALOR 1
+    radio.write(&estadoBotao, sizeof(estadoBotao)); //ENVIA AO RECEPTOR OS DADOS
+  }else{ //SENÃO, FAZ
+        if(digitalRead(pinoBotao) == LOW){ //SE A LEITURA DO PINO FOR IGUAL A LOW, FAZ
+        estadoBotao = 0; //VARIÁVEL RECEBE VALOR 0
+        radio.write(&estadoBotao, sizeof(estadoBotao)); //ENVIA AO RECEPTOR OS DADOS
         }
-    }
   }
-  Serial.println(gotArray[1]);
-  delay(5); //INTERVALO DE 5 MILISSEGUNDOS
 }
